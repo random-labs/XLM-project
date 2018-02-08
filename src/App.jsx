b27ef5837c4774d9d4787567256e4466eb032292
myles <mylesdawson@gmail.com>
2018-02-08 01:09:27 -0800

    Removed state from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  state = {}
   render() {
     return (
-      <div>What the HECC is up</div>
+      <div>What the HECC is up!!!!!!!!!!!</div>
     );
   }
 }
