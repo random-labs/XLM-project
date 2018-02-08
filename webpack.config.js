f580007d4d43da9dcca24fd56e2c2ca7621b66c7
myles <mylesdawson@gmail.com>
2018-02-08 01:05:11 -0800

    Configuration changes: npm run start should work

--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,15 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: [
           'babel-loader',
         ],
       },
     ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
 };
