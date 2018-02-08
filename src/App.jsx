5098a7af0f7017d472e055921e619a113468cffe
myles <mylesdawson@gmail.com>
2018-02-08 00:53:47 -0800

    Updated files to conform to eslint style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {}
   render() {
     return (
       <div>What the HECC is up</div>
     );
   }
-};
+}
 
 export default App;
