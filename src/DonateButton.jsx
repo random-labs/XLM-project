bda73883338fe579fc351cb8adf33e11d90c9b01
vberezny <v.bereznyakov@gmail.com>
2018-02-19 19:18:12 -0800

    added DonateButton, unable to view yet due to docker issues

---
+++ src/DonateButton.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class DonateButton extends Component {
+  render() {
+    return (
+      <button>Donate</button>
+    );
+  }
+}
+
+export default DonateButton;
