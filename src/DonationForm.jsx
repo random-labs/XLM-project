425419369b4cebe12350f11d16062b8133f16efd
vberezny <v.bereznyakov@gmail.com>
2018-02-26 19:40:38 -0800

    minor restructuring in DonationForm

--- src/DonationForm.jsx
+++ src/DonationForm.jsx
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 
 class DonationForm extends Component {
   render(){
-    return(
-       //TODO: decide npm package or build myself
+    return (
+        // TODO: decide npm package or build own form
+        <div>placeholder</div>
     );
   }
 }
